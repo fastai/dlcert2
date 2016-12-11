83bd18a001bc5e37f0d65427ac5fc024700b0bb6
Jeremy Howard <j@fast.ai>
2016-12-11 19:29:29 +0000

    update blog

--- docs/javascripts/libs.js
+++ docs/javascripts/libs.js
@@ -1,12 +1,12 @@
 ---
-# The front matter is required, even it's empty
+# The front matter is required, even its empty
 ---
 
-{% include_relative  "modernizr.foundation.js" %}
-{% include_relative  'jquery-2.1.0.js' %}
-{% include_relative  'terrific-1.1.1.js' %}
-{% include_relative  'jquery.url.js' %}
-{% include_relative  'jquery.cookie.js' %}
-{% include_relative  'json2.js' %}
-{% include_relative  'foundation.js' %}
+{% include_relative  modernizr.foundation.js %}
+{% include_relative  jquery-2.1.0.js %}
+{% include_relative  terrific-1.1.1.js %}
+{% include_relative  jquery.url.js %}
+{% include_relative  jquery.cookie.js %}
+{% include_relative  json2.js %}
+{% include_relative  foundation.js %}
 
